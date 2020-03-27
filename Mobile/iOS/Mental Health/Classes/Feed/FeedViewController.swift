9c7506e3ef401385399c7a73464dda321c630294
mihaibetej <betej.mihai@gmail.com>
2020-03-27 22:42:23 +0200

    Added placeholder VC's for the tabs

---
+++ Mobile/iOS/Mental Health/Classes/Feed/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Mental Health
+//
+//  Created by Mihai Betej on 27/03/2020.
+//  Copyright © 2020 Mihai Betej. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mobile/iOS/Mental Health/Classes/Personal/PersonalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonalViewController.swift
+//  Mental Health
+//
+//  Created by Mihai Betej on 27/03/2020.
+//  Copyright © 2020 Mihai Betej. All rights reserved.
+//
+
+import UIKit
+
+class PersonalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
