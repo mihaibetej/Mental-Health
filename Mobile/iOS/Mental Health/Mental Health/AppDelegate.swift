1348107f1672d1c2cec9359354a8b73f474ecefe
mihaibetej <betej.mihai@gmail.com>
2020-03-27 17:00:07 +0200

    Initial folder struct + empty iOS project

---
+++ Mobile/iOS/Mental Health/Mental Health/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Mental Health
+//
+//  Created by Mihai Betej on 27/03/2020.
+//  Copyright © 2020 Mihai Betej. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
