1047e6cd498f8def9db20ad660b6a1cd7377f99c
Razvan Puscasu <razvan.puscasu24@gmail.com>
2020-04-02 00:19:30 +0300

    fix layout

--- Backend/admin/src/screens/questions/index.js
+++ Backend/admin/src/screens/questions/index.js
@@ -1,4 +1,3 @@
 export { default } from './questions';
-export { default as Question } from './question';
 export { default as AddQuestion } from './add-question';
 export { default as EditQuestion } from './edit-question';
