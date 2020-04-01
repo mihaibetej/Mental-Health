c41ffb0623c248ae586badaf911166c78ecfe977
nrclaudiu@gmail.com <nrclaudiu@gmail.com>
2020-04-01 16:02:10 +0300

    Delete unused admin-web folder

--- admin-web/src/screens/home-page/home-page.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import HomePage from './home-page';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<HomePage />);
-  const text = getByText(/First Page/i);
-  expect(text).toBeInTheDocument();
-});
