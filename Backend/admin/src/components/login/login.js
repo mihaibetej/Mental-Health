c8d6071ac374ca5e36471844a304f1142eff9a61
Rotaru Andrei Liviu <andreirotaru1998@gmail.com>
2020-04-02 20:46:22 +0300

    delete alert() to pass lint

--- Backend/admin/src/components/login/login.js
+++ Backend/admin/src/components/login/login.js
@@ -26,7 +26,6 @@ const Login = () => {
       })
       .catch(() => {
         form.resetFields();
-        alert('Invalid combination of email and password!');
         console.log('display login fail error message');
       });
   };
